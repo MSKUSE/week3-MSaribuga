93c0e410d88834d49c6124c078c1dae60bba9374
MSaribuga <mustafasaribuga2018@gmail.com>
2025-03-06 12:36:18 +0300

    week3

---
+++ week3/src/FindMinimum.java
@@ -0,0 +1,21 @@
+public class FindMinimum {
+    public static void main(String[] args) {
+        int num1 = Integer.parseInt(args[0]);
+        int num2 = Integer.parseInt(args[1]);
+        int num3 = Integer.parseInt(args[2]);
+        if (num1 > num2){
+            if (num2 > num3){
+                System.out.println(num3);
+            } else {
+                System.out.println(num2);
+            }
+        } else {
+            if (num1 > num3){
+                System.out.println(num3);
+            }else {
+                System.out.println(num1);
+            }
+        }
+
+    }
+}
